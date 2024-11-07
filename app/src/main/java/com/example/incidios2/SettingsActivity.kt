e96ae9b6b44c37f8c737cff8167c711889f5ee26
Luis Ignacio Rosales <nacholuispalma@gmail.com>
2024-11-07 12:57:25 -0400

    Actualizacion

--- app/src/main/java/com/example/incidios2/SettingsActivity.kt
+++ app/src/main/java/com/example/incidios2/SettingsActivity.kt
@@ -1,10 +1,12 @@
 package com.example.incidios2
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Switch
 import android.widget.Toast
 import com.example.incidios2.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class SettingsActivity : AppCompatActivity() {
 
@@ -27,6 +29,35 @@ class SettingsActivity : AppCompatActivity() {
                 // Desactivar modo oscuro
                 Toast.makeText(this, "Modo oscuro desactivado", Toast.LENGTH_SHORT).show()
                 // Aquí puedes añadir lógica para desactivar el modo oscuro
+            }
+        }
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigation.selectedItemId = R.id.navigation_settings // Seleccionar el ítem de configuración
+
+        bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_fire -> {
+                    // Navegar a la actividad de fuego
+                    startActivity(Intent(this, FireActivity::class.java))
+                    true
+                }
+                R.id.navigation_news -> {
+                    startActivity(Intent(this, NewsActivity::class.java))
+                    true
+                }
+                R.id.navigation_emergency -> {
+                    startActivity(Intent(this, EmergencyActivity::class.java))
+                    true
+                }
+                R.id.navigation_donation -> {
+                    startActivity(Intent(this, DonationActivity::class.java))
+                    true
+                }
+                R.id.navigation_settings -> {
+                    // Ya estamos en settings
+                    true
+                }
+                else -> false
             }
         }
     }
